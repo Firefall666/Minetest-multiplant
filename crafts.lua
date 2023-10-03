156cae22d1afe385862586bda413e4f0ac51d714
Firefall666 <54312772+Firefall666@users.noreply.github.com>
2023-10-03 09:51:48 +0200

    Update crafts.lua
    Add an optional recipe if Mesecons is used

--- crafts.lua
+++ crafts.lua
@@ -1,10 +1,20 @@
 --Kochen:
-minetest.register_craft({
-    type="cooking",
-    output = "multiplant:seed_multiplant",
-    recipe='group:sapling',
-    cooktime=4,
-})
+if (minetest.get_modpath("mesecons")) then --if Mesecons is active, recipe is already used, so change it to a craft
+	minetest.register_craft({
+	type="shapeless",
+	output="multiplant:seed_multiplant",
+	recipe=({
+		'group:sapling'
+		})
+	})
+else
+	minetest.register_craft({
+		type="cooking",
+		output = "multiplant:seed_multiplant",
+		recipe='group:sapling',
+		cooktime=4,
+	})
+end
 
 minetest.register_craft({
     type="cooking",
