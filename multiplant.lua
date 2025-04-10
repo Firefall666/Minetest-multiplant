231a77cff1b1838ce7284545b22d01b7de60381e
Firefall666 <54312772+Firefall666@users.noreply.github.com>
2025-04-10 19:56:38 +0200

    Added groth on wet soil
    Added option for growing Multiplants on wet soil

--- multiplant.lua
+++ multiplant.lua
@@ -7,7 +7,7 @@ farming.register_plant("multiplant:multiplant", {
 	minlight = 5,
 	maxlight = default.LIGHT_MAX,
 	fertility = {"grassland"},
-	groups = {multiplant = 1, flammable = 4},
+	groups = {multiplant = 1, flammable = 4, growing=1},
 	place_param2 = 3,
 })
 
@@ -22,3 +22,28 @@ minetest.register_abm({
 		minetest.add_item(pos, {name=multiplant.get_random_seed()})
 	end,
 })
+
+if minetest.settings:get_bool("multiplant_enable_grow_on_wet_soil") ~= false then
+
+	minetest.register_abm({
+		nodenames = {"group:field"},
+		neighbors = {"air"},
+		interval = 50,
+		chance = 15,
+		catch_up = false,
+
+		action = function(pos, node)
+
+			if minetest.find_node_near(pos, 4, {"farming:scarecrow_bottom"}) then
+				return
+			end
+
+			pos.y = pos.y + 1
+
+			if minetest.get_node(pos).name == "air" then
+				minetest.set_node(pos, {name = "multiplant:multiplant_1", param2 = 2})
+			end
+		end
+	})
+end
+
